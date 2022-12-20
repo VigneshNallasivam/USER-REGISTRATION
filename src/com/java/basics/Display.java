04e54c489b715008b2ac9fa783868805c526ae53
VigneshNallasivam <vickyshanestark@gmail.com>
2022-12-20 12:16:21 +0530

    Added : Displaying the Welcome Message for User Registration Program

---
+++ src/com/java/basics/Display.java
@@ -0,0 +1,11 @@
+package com.java.basics;
+
+public class Display 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to User Registration Simulation Program");
+
+	}
+
+}
